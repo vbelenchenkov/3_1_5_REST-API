828407c31a0bb63d720e176ec412684f8d42d971
viacheslav belenchenkov <belenchenkov@gmail.com>
2026-05-06 22:15:08 +0300

    final solution of the task

--- src/main/java/ru/kata/spring/rest_template/service/RestClient.java
+++ src/main/java/ru/kata/spring/rest_template/service/RestClient.java
@@ -10,13 +10,15 @@ public class RestClient {
         String url = "http://94.198.50.185:7081/api/users";
 
         ResponseEntity<String> getResponse = restTemplate.getForEntity(url, String.class);
-        System.out.println("Users list: " + getResponse.getBody());
+        System.out.println("===INITIAL USERS LIST===");
+        System.out.println(getResponse.getBody());
+        System.out.println();
 
         String setCookie = getResponse.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
         System.out.println("Set-Cookie: " + setCookie);
-
         String sessionId = setCookie.split(";")[0];
         System.out.println("Session ID: " + sessionId);
+        System.out.println();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -27,22 +29,42 @@ public class RestClient {
 
         ResponseEntity<String> postResponse = restTemplate.postForEntity(url, postRequest, String.class);
         String part1 = postResponse.getBody();
+        System.out.println("===AFTER POST STATEMEN (James Brown)===");
         System.out.println("First part: " + part1);
+
+        ResponseEntity<String> afterPostResponse = restTemplate.exchange(
+                url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        System.out.println("Users list after POST:");
+        System.out.println(afterPostResponse.getBody());
+        System.out.println();
 
         User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 30);
         HttpEntity<User> putRequest = new HttpEntity<>(updatedUser, headers);
 
-        ResponseEntity<String> putResponse = restTemplate.exchange(url, HttpMethod.PUT, putRequest, String.class);
+        ResponseEntity<String> putResponse = restTemplate.exchange(
+                url, HttpMethod.PUT, putRequest, String.class);
         String part2 = putResponse.getBody();
+        System.out.println("===AFTER PUT STATEMEN (Thomas Shelby)===");
         System.out.println("Second part: " + part2);
+        ResponseEntity<String> afterPutResponse = restTemplate.exchange(
+                url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        System.out.println("Users list after PUT:");
+        System.out.println(afterPutResponse.getBody());
+        System.out.println();
 
         String deleteUrl = url + "/3";
         HttpEntity<Void> deleteRequest = new HttpEntity<>(headers);
 
-        ResponseEntity<String> deleteResponse = restTemplate.exchange(deleteUrl, HttpMethod.DELETE,
-                deleteRequest, String.class);
+        ResponseEntity<String> deleteResponse = restTemplate.exchange(
+                deleteUrl, HttpMethod.DELETE, deleteRequest, String.class);
         String part3 = deleteResponse.getBody();
+        System.out.println("===AFTER DELETE STATEMEN (id=3 user deletion)===");
         System.out.println("Third part: " + part3);
+        ResponseEntity<String> afterDeleteResponse = restTemplate.exchange(
+                url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        System.out.println("Final users list:");
+        System.out.println(afterDeleteResponse.getBody());
+        System.out.println();
 
         String resultCode = part1 + part2 + part3;
         System.out.println("==========================================");
